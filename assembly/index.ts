b8d92fa2319b749896e3cbf616951bfbad085833
SliceWire <greatlyrejoice@gmail.com>
2021-03-17 12:05:35 -0700

    Update error

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,7 +3,7 @@ export function search(array: Array<string>, search: string): Array<string> {
   const query = search.toLowerCase().trim()
   for (let i = 0; i < array.length; i++) {
     const element = array[i].toLowerCase()
-    if (element.includes(query)) results.push(element)
+    if (element === query) results.push(element)
   }
   return results
 }
